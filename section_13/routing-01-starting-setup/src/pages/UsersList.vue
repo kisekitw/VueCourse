654c0107b21433ca19b37ac0bd5f06d87a440177
Xavier Lin <kisekitw@gmail.com>
2020-11-22 08:59:42 +0800

    vue courese

---
+++ section_13/routing-01-starting-setup/src/pages/UsersList.vue
@@ -0,0 +1,62 @@
+<template>
+<button @click="confirmInput">Confirm</button>
+<button @click="saveChanges">Save Changes</button>
+<ul>
+    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+</ul>
+</template>
+
+<script>
+import UserItem from '../components/users/UserItem';
+
+export default {
+    data() {
+        return {
+            changesSaved: false
+        };
+    },
+    components: {
+        UserItem
+    },
+    inject: ['users'],
+    methods: {
+        confirmInput() {
+            this.$router.push('/teams');
+        },
+        saveChanges() {
+            this.changesSaved = true;
+        }
+    },
+    beforeRouteEnter(to, from, next) {
+        console.log('UserList cmp beforeRouterEnter');
+        console.log(to, from, next);
+    },
+    beforeRouteLeave(to, from, next) {
+        // ...
+        console.log('UsersList Cmp beforeRouteLeave');
+        console.log(to, from);
+
+        if (this.changesSaved) {
+            next();
+        } else {
+            const userWantsToLeave = confirm(
+                'Are you sure? You got unsaved changes!'
+            );
+            next(userWantsToLeave);
+        }
+        next();
+    },
+    unmounted() {
+        console.log('unmounted');
+    }
+};
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 2rem auto;
+    max-width: 20rem;
+    padding: 0;
+}
+</style>
